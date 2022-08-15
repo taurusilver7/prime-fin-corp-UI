c28724a4d5a5b906db053114e445b77fd6352e86
taurusilver7 <prasad20697@gmail.com>
2022-08-15 17:54:00 +0530

    components mounted in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Business, CardDeal, Navbar } from "./components";
+import styles from "./styles";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="bg-primary w-full overflow-hidden">
+      <div className="flex items-center justify-center px-6 sm:px-16">
+        <div className="xl:max-w-[1280px] w-full"><Navbar /></div>
+      </div>
 
-export default App
+      <div className="bg-primary w-full items-start justify-center">
+        <div className="xl:max-w-[1280px] w-full">Hero component</div>
+      </div>
+
+      <div className="bg-primary w-full px-6 sm:px-16 items-start justify-center">
+        <div className="xl:max-w-[1280px] w-full">
+          Stats
+          <Business />
+          component Billings Testmonials Clients CTA Footer
+          <CardDeal />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
