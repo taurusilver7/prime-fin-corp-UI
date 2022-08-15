d934827f06088acf5b25d069fe785c17630190ac
taurusilver7 <prasad20697@gmail.com>
2022-08-15 23:11:47 +0530

    component congregate file

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import Business from "./Business";
 import CardDeal from "./CardDeal";
 import Navbar from "./Navbar";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";
 
-export { Business, CardDeal, Navbar };
+export { Business, CardDeal, Navbar, Hero, GetStarted, Stats };
