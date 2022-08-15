ddec0bde210829a68b77189e52b8eed7a0cc9a10
taurusilver7 <prasad20697@gmail.com>
2022-08-15 23:08:53 +0530

    navbar hero & stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import { Business, CardDeal, Navbar } from "./components";
+import { Business, CardDeal, Hero, Navbar, Stats } from "./components";
 import styles from "./styles";
 
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className="flex items-center justify-center px-6 sm:px-16">
-        <div className="xl:max-w-[1280px] w-full"><Navbar /></div>
+        <div className="xl:max-w-[1280px] w-full">
+          <Navbar />
+        </div>
       </div>
 
       <div className="bg-primary w-full items-start justify-center">
-        <div className="xl:max-w-[1280px] w-full">Hero component</div>
+        <div className="xl:max-w-[1280px] w-full">
+          <Hero />
+        </div>
       </div>
 
       <div className="bg-primary w-full px-6 sm:px-16 items-start justify-center">
         <div className="xl:max-w-[1280px] w-full">
-          Stats
+          <Stats />
           <Business />
           component Billings Testmonials Clients CTA Footer
           <CardDeal />
